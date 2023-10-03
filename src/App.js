ebc1a974e2214644b11e595942f16afad7e7719d
Muhammad Rafi Fahrezi <rafi05fahrezi@gmail.com>
2023-10-03 17:27:09 +0700

    02/10/2023

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./components/navbar/navbar";
+import Intro from "./components/intro/intro";
+import Skills from "./components/skills/skills";
+import Works from "./components/works/works";
+import Contact from "./components/contact/contact";
+import Footer from "./components/footer/footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Intro />
+      <Skills />
+      <Works />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/contact/contact.js
@@ -0,0 +1,65 @@
+import React, { useRef } from 'react';
+import './contact.css';
+import Walmart from '../../assets/walmart.png';
+import Adobe from '../../assets/adobe.png';
+import Microsoft from '../../assets/microsoft.png';
+import Facebook from '../../assets/facebook.png';
+import FacebookIcon from '../../assets/facebook-icon.png';
+import YoutubeIcon from '../../assets/youtube.png';
+import TwitterIcon from '../../assets/twitter.png';
+import InstagramIcon from '../../assets/instagram.png';
+import emailjs from '@emailjs/browser';
+
+const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_5sgrqf8', 'template_ckjicg8', form.current, 'Yvpk4GgadTr9Zhgwp')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+          alert('Email Sent !');
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <section id="contactPage">
+      <div id="clients">
+        <h1 className="contactPageTitle">My Client</h1>
+        <p className="clientDesc">
+          I have had the opportunity to work with a diverse group of companies.
+          Some of the notable companies I have worked with includes.
+        </p>
+        <div className="clientImgs">
+          <img src={Walmart} alt="Client" className="clientImg" />
+          <img src={Adobe} alt="Client" className="clientImg" />
+          <img src={Microsoft} alt="Client" className="clientImg" />
+          <img src={Facebook} alt="Client" className="clientImg" />
+        </div>
+      </div>
+      <div id="contact">
+        <h1 className="contactPageTitle">Contact Me</h1>
+        <span className="contactDesc">Please fill out the form below to discuss any work opportunities.</span>
+        <form className="contactForm" ref={form} onSubmit={sendEmail}>
+          <input type="text" className='name' placeholder='Your Name' name='your_name' />
+          <input type="email" className="email" placeholder='Your Email' name='your_email' />
+          <textarea className='msg' name="message" rows="5" placeholder='Your Message'></textarea>
+          <button type='submit' value='Send' className="submitBtn">Submit</button>
+          <div className="links">
+            <img src={FacebookIcon} alt="Facebook" className="link" />
+            <img src={TwitterIcon} alt="Twitter" className="link" />
+            <img src={YoutubeIcon} alt="Youtube" className="link" />
+            <img src={InstagramIcon} alt="Instagram" className="link" />
+          </div>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
